e35846c6931a91128fcae841fdbaee74ce28f04d
LeonasOnGitHub <103933123+LeonasOnGitHub@users.noreply.github.com>
2022-11-17 19:58:04 +0100

    Uebung 6 beendet

--- src/main/java/intervall/Hauptprogramm.java
+++ src/main/java/intervall/Hauptprogramm.java
@@ -1,19 +1,49 @@
 package intervall;
 
-import javax.sound.midi.Soundbank;
+
+import java.sql.Timestamp;
+import java.util.Date;
+import java.sql.Time;
 
 public class Hauptprogramm {
     public static void main(String[] args) {
-        String siog = "A";
-        String siug = "D";
 
-        Intervall<String> sIntervall = new Intervall<>(siog, siug);
+        //Test mit String Intervall
+        Intervall<String> sIntervall = new Intervall<>("A", "D");
         System.out.println("Das Intervall ist leer: " + sIntervall.istLeer());
-        System.out.println("Das Intervall enthält ein B: " + sIntervall.enthaelt("B"));
-        System.out.println("Das Intervall enthält ein X: " + sIntervall.enthaelt("X"));
+        System.out.println("Das Intervall enthaelt ein B: " + sIntervall.enthaelt("B"));
+        System.out.println("Das Intervall enthaelt ein X: " + sIntervall.enthaelt("X"));
         Intervall<String> newSIntervall = sIntervall.schnitt(new Intervall<>("B", "E"));
         System.out.println("Das schnitt Intervall hat die Menge von: " + newSIntervall.untereGrenze + "-" + newSIntervall.obereGrenze);
 
+        //test mit Date schnitt Date Intervall
+        System.out.println("-------------------Date schnitt Date-------------------");
+        Date date1 = new Date(200, 10, 1);
+        Date date2 = new Date(201, 10, 1, 10, 55, 10);
+        Date date3 = new Date(200, 11, 1);
+        Date date4 = new Date(211, 8, 1);
+        Intervall<Date> dateIntervall = new Intervall<>(date1, date2);
+        Intervall<Date> newDateIntervall = dateIntervall.schnitt(new Intervall<>(date3, date4));
+        System.out.println("Das schnitt Intervall hat die Menge von: " + newDateIntervall.untereGrenze + "-" + newDateIntervall.obereGrenze);
 
+        //test mit Date schnitt Time Intervall
+        System.out.println("-------------------Date schnitt Time-------------------");
+        Time time1 = new Time(3600000L);
+        Time time2 = new Time(39600000L);
+        Intervall<Time> timeIntervall = new Intervall<>(time1, time2);
+        Intervall<Date> newDateTimeIntervall = dateIntervall.schnitt(timeIntervall);
+        System.out.println("Das schnitt Intervall hat die Menge von: " + newDateTimeIntervall.untereGrenze + "-" + newDateTimeIntervall.obereGrenze);
+
+        //test ob TimeStamp in Date und Time enthalten ist
+        System.out.println("----------------TimeStamp in Date/time-----------------");
+        Timestamp ts=new Timestamp(13600000L);
+        System.out.println("Das Time Intervall enthaelt den TS: " + timeIntervall.enthaelt(ts));
+        System.out.println("Das Date Intervall enthaelt den TS: " + dateIntervall.enthaelt(ts));
+
+
+       //FEHLER: Intervall<Number> falsch = new Intervall<>("A", "B");
+        Intervall<Double> zahlen = new Intervall<>(1.2, 3.4);
+        Intervall<String> texte = new Intervall<>("a", "b");
+        //FEHLER: zahlen.schnitt(texte);
     }
 }
--- src/main/java/intervall/Intervall.java
+++ src/main/java/intervall/Intervall.java
@@ -1,5 +1,8 @@
 package intervall;
 
+/*
+Diese Klasse stellt ein Intervall zwischen zwei beliben werten dar
+ */
 public class Intervall<T extends Comparable<? super T>> {
     public T obereGrenze;
     public T untereGrenze;
@@ -11,16 +14,31 @@ public class Intervall<T extends Comparable<? super T>> {
 
     /**
      * Überptüft ob der Intervall leer ist
+     *
      * @return true, wenn der intervall leer ist
      */
     public boolean istLeer() {
         return obereGrenze.compareTo(untereGrenze) < 1;
     }
 
-    public <T1 extends T> boolean enthaelt(T1 wert) {
+    /**
+     * gibt an ob ein wert sich inerhalb this Intervalss befindet
+     *
+     * @param wert den zu überprüfenden wert
+     * @param <T1>
+     * @return ob der wert in dem this Intervall ist
+     */
+    public <T1 extends Comparable<? super T>> boolean enthaelt(T1 wert) {
         return wert.compareTo(obereGrenze) < 1 && wert.compareTo(untereGrenze) > 0;
     }
 
+    /**
+     * Macht asu this und einem weiteren intervall ein Schnittintervall
+     *
+     * @param anderes das weitere Intervall
+     * @param <T2>
+     * @return das neue Schnittintervall
+     */
     public <T2 extends T> Intervall<T> schnitt(Intervall<T2> anderes) {
         T schnittObereGrenze;
         T schnittUntereGrenze;
@@ -30,9 +48,9 @@ public class Intervall<T extends Comparable<? super T>> {
             schnittObereGrenze = anderes.obereGrenze;
         }
         if (this.untereGrenze.compareTo(anderes.untereGrenze) >= 0) {
-            schnittUntereGrenze = this.obereGrenze;
+            schnittUntereGrenze = this.untereGrenze;
         } else {
-            schnittUntereGrenze = anderes.obereGrenze;
+            schnittUntereGrenze = anderes.untereGrenze;
         }
 
         return new Intervall<>(schnittUntereGrenze, schnittObereGrenze);
